0225bcef0a0dfe90259b04eeaa0abd99fa76106b
SavchenkoIvan24022022 <167643867+SavchenkoIvan24022022@users.noreply.github.com>
2024-10-20 13:06:48 +0300

    add css

--- src/main.js
+++ src/main.js
@@ -7,21 +7,49 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { createGallery } from './js/render-functions';
 
 import { fetchImages } from './js/pixabay-api';
-import { document } from 'postcss';
+
 console.log(document);
 
-const searchForm = document.querySelector(".form");
-const galleryList = document.querySelector(".imagebox");
+const searchForm = document.querySelector('.form');
+const galleryList = document.querySelector('.imagebox');
 
 searchForm.addEventListener('submit', onSubmit);
 function onSubmit(eve) {
   eve.preventDefault();
+  galleryList.innerHTML = '';
   const query = eve.target.elements.input.value.trim();
+  if (!query) {
+    iziToast.warning({
+      title: 'Hey',
+      message: "It's alive! Congratulations!",
+    });
+    return;
+}
+
   fetchImages(query).then(response => {
+    if (response.hits.length === 0) {
+      iziToast.error({
+        title: 'Hey',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
     console.log(response);
 
     const markUp = createGallery(response.hits);
-    // galleryList.innerHTML = markUp;
+    galleryList.innerHTML = markUp;
     console.log(markUp);
-  });
+  }
+ ) .catch((error)=>{
+    iziToast.error({
+        title: 'Hey',
+        message:
+          `${error.message}`,
+      });
+
+ })
+ 
+
+
 }
